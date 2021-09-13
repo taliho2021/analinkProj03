df480c8eb4c61a0482d932a8d42185ebd131dac7
taliho2021 <tyyo1980@gmail.com>
2021-09-13 12:35:27 -0400

    2nd commit

---
+++ src/app/components/observ/observ.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-observ',
+  templateUrl: './observ.component.html',
+  styleUrls: ['./observ.component.scss']
+})
+
+export class ObservComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
